f83becac483378bbd5ce5b01ee6bbf0050acf799
H.L.Hsieh <hsieh.332@osu.edu>
2021-07-03 21:13:37 +0000

    Update Assignment

---
+++ SecondAssignment_python_sys.py
@@ -0,0 +1,34 @@
+## import package
+from sklearn.linear_model import LinearRegression
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sys
+
+## load data
+df = pd.read_csv(sys.argv[1])
+
+## plot scatter
+plt.scatter(x=df.x, y=df.y)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.savefig("py_orig_sys.png")
+
+## model the data
+linear_regressor = LinearRegression()
+X = df.iloc[:, 1].values.reshape(-1, 1)
+Y = df.iloc[:, 0].values.reshape(-1, 1)
+fit = linear_regressor.fit(X, Y)
+
+## predict y
+Y_pred = linear_regressor.predict(X)
+
+## plot scatter with predicting value
+plt.scatter(X, Y)
+plt.plot(X, Y_pred, color='red')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+plt.savefig("py_lm_sys.png")
